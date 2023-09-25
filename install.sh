0bdf57774057521dfa275dff0f2ca300eacba9a4
·° ∴⊶АО⊷ ∴°· <34207320+AoAnima@users.noreply.github.com>
2023-09-25 16:35:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
-
+CNI_PLUGINS_VERSION="v1.3.0"
+CRICTL_VERSION="v1.28.0"
+ARCH="amd64"
+DEST="/opt/cni/bin"
+DOWNLOAD_DIR="/usr/local/bin"
+RELEASE="$(curl -sSL https://dl.k8s.io/release/stable.txt)"
+RELEASE_VERSION="v0.15.1"
